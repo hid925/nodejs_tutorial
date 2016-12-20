115c3e7dbddcfcb96425df8858ce53b31b37e5ff
Byoungyong <hid925@gmail.com>
2016-12-20 20:15:25 +0900

    get,post방식

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
-  app.locals.pretty = true;
+app.locals.pretty = true;
 
 app.set('view engine', 'jade');
 app.set('views','./views')
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.get('/form_receiver', function(req,res){
+    var title = req.query.title;
+    var description = req.query.description;
+    res.send(title+','+description);
+})
+
+app.post('/form_receiver', function(req,res){
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send(title+','+description);
+})
+
+app.get('/form',function(req,res){
+  res.render('form');
+
+})
 
 app.get('/topic/:id', function(req, res) {
   var topics = [
@@ -15,12 +35,13 @@ app.get('/topic/:id', function(req, res) {
   ];
 
   var output = `
-  <a href = "/topic?id=0">JavaScript</a><br>
-  <a href = "/topic?id=1">Nodejs</a><br>
-  <a href = "/topic?id=2">Express</a><br><br>
+  <a href = "/topic/0">JavaScript</a><br>
+  <a href = "/topic/1">Nodejs</a><br>
+  <a href = "/topic/2">Express</a><br><br>
 
   ${topics[req.params.id]}
   `
+
 
   res.send(output);
 })
