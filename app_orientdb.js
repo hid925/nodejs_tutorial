f10cad3653381e8e5d0b61da90e73aae8209bbb1
Byoungyong <hid925@gmail.com>
2016-12-22 18:57:04 +0900

    u d with orientdb

--- app_orientdb.js
+++ app_orientdb.js
@@ -22,10 +22,6 @@ app.set('view engine', 'jade'); // 제이드 셋팅
 app.get('/topic/add', function(req, res) {
   var sql = 'SELECT FROM topic';
   db.query(sql).then(function(topics){
-    if(topics.length === 0){
-      console.log('No record');
-      res.status(500).send('Internal Sever Error');
-    }
     res.render('add',{topics:topics});
   });
 });
@@ -46,6 +42,62 @@ app.post('/topic/add',function(req, res) {
   });
 });
 
+app.get('/topic/:id/edit', function(req, res) {
+  var sql = 'SELECT FROM topic';
+  var id = req.params.id;
+  db.query(sql).then(function(topics){
+    var sql = 'SELECT FROM topic  WHERE @rid=:rid';
+    db.query(sql,{params:{rid:id}}).then(function(topic){
+    //  console.log(topic[0]);
+      res.render('edit',{topics:topics, topic:topic[0]});  //{속성(디비):값()}
+    });
+  });
+});
+
+app.post('/topic/:id/edit', function(req, res) {
+  var sql = 'UPDATE topic SET title=:t, description=:d, author=:a WHERE @rid=:rid';
+  var id = req.params.id;
+  var title = req.body.title;
+  var desc =req.body.description;
+  var author = req.body.author;
+  db.query(sql,{
+    params:{
+      t:title,
+      d:desc,
+      a:author,
+      rid:id
+    }
+  }).then(function(topics){
+    //  console.log(topic[0]);
+    res.redirect('/topic/'+encodeURIComponent(id));
+  });
+});
+
+
+app.get('/topic/:id/delete', function(req, res) {
+  var sql = 'SELECT FROM topic';
+  var id = req.params.id;
+  db.query(sql).then(function(topics){
+    var sql = 'SELECT FROM topic  WHERE @rid=:rid';
+    db.query(sql,{params:{rid:id}}).then(function(topic){
+    //  console.log(topic[0]);
+      res.render('delete',{topics:topics, topic:topic[0]});  //{속성(디비):값()}
+    });
+  });
+});
+app.post('/topic/:id/delete',function(req, res) {
+  var sql = 'DELETE FROM topic WHERE @rid=:rid';
+  var id = req.params.id;
+  db.query(sql, {
+    params:{
+      rid:id
+    }
+  }).then (function(topics){
+    res.redirect('/topic/');
+  });
+});
+
+
 app.get(['/topic', '/topic/:id'], function(req, res) {      //목록
   var sql = 'SELECT FROM topic';
   db.query(sql).then(function(topics){
@@ -61,7 +113,6 @@ app.get(['/topic', '/topic/:id'], function(req, res) {      //목록
     }
   });
 });
-
 
 app.listen(3000, function() {     //3000포트로 연결
   console.log('Conneted, 3000 port!');
